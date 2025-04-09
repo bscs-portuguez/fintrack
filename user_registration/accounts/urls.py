0aca29829c7ef6942e0ca77860853949f9dc224e
John Matthew Perez <johnmatthewprz@gmail.com>
2025-04-09 15:21:09 +0800

    Merge layouts

--- user_registration/accounts/urls.py
+++ user_registration/accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('welcome/', views.welcome, name='welcome'),
-    path('logout/', views.logout_view, name='logout'),  # Add this line
+    path('logout/', views.logout_view, name='logout'),
+    path('encode/', views.encode, name='encode'),
 ]
